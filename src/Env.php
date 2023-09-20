30f629db078fa7f6b1db046b39d5f626f2f96642
Tekin Tian <tekintian@gmail.com>
2023-09-20 15:44:26 +0800

    数据类型过滤和转换功能增加
    int, float, bool, url ,ip regexp and string value filter
    
    Signed-off-by: Tekin Tian <tekintian@gmail.com>

--- src/Env.php
+++ src/Env.php
@@ -111,10 +111,38 @@ class Env {
 	 *
 	 * @param  string  $key
 	 * @param  mixed  $default
+	 * @param  string  $type default str no value filter, support int, float, bool, url ,ip regexp and string value filter
 	 * @return mixed
 	 */
-	public static function get($key, $default = null) {
-		return self::getOption($key)->getOrCall(fn() => $default instanceof Closure ? $default(...$args) : $default);
+	public static function get($key, $default = null, $type = 'str') {
+		$val = self::getOption($key)->getOrCall(fn() => $default instanceof Closure ? $default(...$args) : $default);
+		if ($type == 'str' || null === $val) {
+			return $val;
+		}
+		switch ($type) {
+		case 'int':
+			$val = intval(filter_var($val, FILTER_SANITIZE_NUMBER_INT));
+			break;
+		case 'float':
+			$val = floatval(filter_var($val, FILTER_SANITIZE_NUMBER_FLOAT));
+			break;
+		case 'boolean':
+		case 'bool':
+			$val = boolval(filter_var($val, FILTER_VALIDATE_BOOLEAN));
+			break;
+		case 'url':
+			$val = filter_var($val, FILTER_VALIDATE_URL);
+			break;
+		case 'ip':
+			$val = filter_var($val, FILTER_VALIDATE_IP);
+			break;
+		case 'regexp':
+			$val = filter_var($val, FILTER_VALIDATE_REGEXP);
+			break;
+		default:
+			$val = strval(filter_var($val, FILTER_SANITIZE_STRING));
+		}
+		return $val;
 	}
 
 	/**
